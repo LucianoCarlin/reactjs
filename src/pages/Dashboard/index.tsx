31a46ba122011d8dedbf55c773ac04081d5c9a26
windel <windel@debianwindel.windel.local>
2021-07-29 17:19:42 -0300

    primeira parte curso

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Title } from './styles';
+import logo from '../../assets/logo.svg';
+
+export const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logo} alt="GitCollection" />
+      <Title>Dashboard</Title>
+    </>    
+  )
+
+};
+
