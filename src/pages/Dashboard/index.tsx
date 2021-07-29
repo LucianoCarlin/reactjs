2dea1bcbd22f17e48ee85fcd251214fa787a2720
windel <windel@debianwindel.windel.local>
2021-07-29 17:46:24 -0300

    Ajustes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { Title, Form } from './styles';
 import logo from '../../assets/logo.svg';
 
 export const Dashboard: React.FC = () => {
   return (
     <>
       <img src={logo} alt="GitCollection" />
-      <Title>Dashboard</Title>
+      <Title>Catálogo de repositórios do Github</Title>
+
+      <Form>
+        <input placeholder="username/repository_name" />
+        <button type="submit">Buscar</button>
+      </Form>
     </>    
   )
 
