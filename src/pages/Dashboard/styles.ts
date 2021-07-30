81d405b37be0ac03f3a331df4f40ad1ac1fe8991
windel <windel@debianwindel.windel.local>
2021-07-30 15:03:34 -0300

    estilizaçao

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,11 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import { shade } from "polished";
+
+interface FormProps {
+  hasError: boolean;
+}
+
 
 export const Title = styled.h1`
   font-size: 3rem;
@@ -10,7 +15,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form `
+export const Form = styled.form<FormProps> `
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -23,6 +28,10 @@ export const Form = styled.form `
     border-radius: 5px 0px 0px 5px;
     color: #3a3a3a;
     border-right: 0;
+
+    ${props => props.hasError && css `
+      border-color: #c53030;
+    `}
 
     &::placeholder{
       color: #a8a8b3;
@@ -44,3 +53,61 @@ export const Form = styled.form `
     }
   }
 `;
+
+export const Repos = styled.div `
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(6px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong{
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
+
+export const Error = styled.span `
+  background: 'rgb(255, 102, 102)';
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
+`;
+
--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,6 +1,101 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
 
-export const Repo: React.FC = () => {
-  return <h1>Repositorios</h1>;
+import { api } from '../../Services/api';
+
+import { Header, RepoInfo, Issues } from './styles';
+import logo from '../../assets/logo.svg'
+
+interface RepositoryParams {
+  repository: string;
+}
+
+interface GithubRepository {
+  full_name: string;
+  description: string;
+  forks_count: number;
+  open_issues_count: number;
+  stargazers_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface GithubIssue {
+  id: string;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  }
+}
+
+export const Repo: FC = () => {
+  const [repository, setRepository] = useState<GithubRepository | null>(null);
+  const [issues, setIssues] = useState<GithubIssue[]>([]);
+
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(response => setRepository(response.data));
+
+    api.get(`repos/${params.repository}/issues`).then(response => setIssues(response.data));
+
+  }, [params.repository]);
+
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Gitcollection" />
+        <Link to="/">
+          <FiChevronLeft />
+          Voltar
+        </Link>
+      </Header>
+
+      {repository && (
+        <RepoInfo>
+        <header>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <div>
+            <strong>{repository.full_name}</strong>
+            <p>{repository.description}</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>{repository.stargazers_count}</strong>
+            <span>Stars</span>
+          </li>
+
+          <li>
+            <strong>{repository.forks_count}</strong>
+            <span>Forks</span>
+          </li>
+
+          <li>
+            <strong>{repository.open_issues_count}</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepoInfo>
+      )}
+
+      <Issues>
+        {issues.map(issue => (
+          <a href={issue.html_url} key={issue.id}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Issues>
+    </>
+  );
 };
 
